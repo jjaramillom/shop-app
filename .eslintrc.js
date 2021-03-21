45d7c1ec4960f5ad3c2feaefb5758f68fb1dcec1
Jacobo Jaramillo <jjaramillom@unal.edu.co>
2021-03-21 14:56:17 +0100

    set linter. add git pre-commit hooks. install dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  env: {
+    node: true,
+    es6: true,
+    'react-native/react-native': true,
+  },
+  settings: {
+    'import/extensions': ['.ts'],
+    react: {
+      version: 'detect',
+    },
+    'import/ignore': ['node_modules'],
+  },
+  plugins: ['@typescript-eslint', 'import', 'filenames', 'prettier', 'react', 'react-native'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 2,
+    '@typescript-eslint/no-unused-vars': ['error', { args: 'none' }],
+    '@typescript-eslint/no-use-before-define': 0,
+    'filenames/match-exported': [2, ['pascal', 'camel']],
+    'import/newline-after-import': 2,
+    'import/no-duplicates': 2,
+    'import/order': [
+      2,
+      {
+        'newlines-between': 'always',
+        groups: ['external'],
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
+    'import/named': 2,
+    'react-native/no-unused-styles': 2,
+    'react-native/split-platform-components': 2,
+    'react-native/no-inline-styles': 2,
+  },
+};
---
+++ .lintstagedrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  'src/**/*.ts': ['npm run lint:ts'],
+  'src/**/*.tsx': ['npm run lint:tsx'],
+};
